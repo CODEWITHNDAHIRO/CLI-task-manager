86b6d3f4422c499d409c1cd4916dac1a55913171
CODEWITHNDAHIRO <ndahiro.putinejonathan@iu-study.org>
2026-05-22 19:05:12 +0200

    first commit, simple features

---
+++ task_manager.py
@@ -0,0 +1,10 @@
+tasks = []
+while True:
+    print("\n--- Task Manager ---")
+    print("1. view tasks")
+    print("2. Exit")
+    
+    choice = input("Enter your choice: ")
+    if choice == "2":
+        print("Exiting Task Manager!")
+        break
